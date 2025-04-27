ca2129d1db8f1b72f8ca5cb29c0f79584b7fceca
Gusto <lhg0616@gmail.com>
2025-04-27 20:44:35 +0900

    ant-design-vue 설치

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import StyleProvider from "./components/StyleProvider.vue";
 </script>
 
 <template>
-  <div>
-    style-repo
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <StyleProvider>
+    <div>
+      style-repo
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+    <a-button>button</a-button>
+  </StyleProvider>
 </template>
 
 <style scoped>
